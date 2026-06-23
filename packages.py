0dd23a1e196098bedb0cd64486d08f92a73b4f87
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-24 04:17:28 +0700

    Create packages.py

---
+++ packages.py
@@ -0,0 +1,29 @@
+PACKAGE_MAP = {
+    "1bulan": {
+        "label": "1 Bulan",
+        "price": 199000,
+        "days": 30
+    },
+    "6bulan": {
+        "label": "6 Bulan",
+        "price": 549000,
+        "days": 180
+    },
+    "12bulan": {
+        "label": "12 Bulan",
+        "price": 999999,
+        "days": 365
+    },
+    "permanent": {
+        "label": "Permanent",
+        "price": 1999999,
+        "days": None
+    },
+
+    # 🆕 Trial 60 menit
+    "trial": {
+        "label": "Trial (60 Menit)",
+        "price": 0,
+        "duration_minutes": 60
+    }
+}
