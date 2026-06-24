027bd6998b8017b41f3de3d3fc61027817a6de10
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-24 08:05:08 +0700

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,6 +1,18 @@
 PACKAGE_MAP = {
-    "1bulan": {"label": "1 Bulan", "price": 199000},
-    "6bulan": {"label": "6 Bulan", "price": 549000},
-    "12bulan": {"label": "12 Bulan", "price": 999000},
-    "permanent": {"label": "Permanent", "price": 1999000},
+    "1bulan": {
+        "label": "1 Bulan",
+        "price": 199000
+    },
+    "6bulan": {
+        "label": "6 Bulan",
+        "price": 549000
+    },
+    "12bulan": {
+        "label": "12 Bulan",
+        "price": 999000
+    },
+    "permanent": {
+        "label": "Permanent",
+        "price": 1999000
+    }
 }
