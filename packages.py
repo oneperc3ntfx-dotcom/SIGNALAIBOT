452b7988b8d217a2c51b69483cdf57d3e4655d81
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-11 15:28:22 +0700

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,18 +1,4 @@
-PACKAGE_MAP = {
-    "1bulan": {
-        "label": "1 Bulan",
-        "price": 499000
-    },
-    "6bulan": {
-        "label": "6 Bulan",
-        "price": 999000
-    },
-    "12bulan": {
-        "label": "12 Bulan",
-        "price": 1999000
-    },
-    "permanent": {
-        "label": "Permanent",
-        "price": 4990000
-    }
-}
+InlineKeyboardButton(
+    text="👑 LIFETIME ACCESS • Rp150K",
+    callback_data="pkg_permanent"
+)
