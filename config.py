5a5befa53dbf8d71760e25e7a97eaa33fb53c956
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-20 14:14:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 GROUP_ID = int(os.getenv("GROUP_ID"))
 
-BANK_INFO = """
-🏦 Bank SMBC
-👤 a/n Yuriandi Arma
-💳 90240573080
+MERCHANT_NAME=KOLEKSIKAMUTAS
 
-🏦 Bank CIMB NIAGA
-👤 a/n Yuriandi Arma
-💳 708420455200
+QRIS_IMAGE=assets/qris.jpg
 """
