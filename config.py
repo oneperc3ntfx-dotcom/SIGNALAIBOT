8c727bb7467e92b0f0bd3351349bbedfc3cf72ac
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-07-20 14:19:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
 GROUP_ID = int(os.getenv("GROUP_ID"))
 
-MERCHANT_NAME=KOLEKSIKAMUTAS
+BANK_INFO = """
+💳 Pembayaran QRIS
 
-QRIS_IMAGE=assets/qris.jpg
+Silakan scan QRIS yang diberikan bot.
+
+Merchant :
+KOLEKSIKAMUTAS
 """
